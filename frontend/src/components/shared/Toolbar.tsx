5c44f479a373b368ce44c02e1fbf3e69826483d9
r.organ <r.organ@dan-it.com.ua>
2025-05-11 09:43:38 +0300

    toolbar component added, sort updated

---
+++ frontend/src/components/shared/Toolbar.tsx
@@ -0,0 +1,47 @@
+import { EnumItem, FilterOption } from "@/types/shared.types";
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+import SearchInput from "./SearchInput";
+import Sort from "./Sort";
+import FilterDropdown from "./FilterDropdown";
+
+interface ToolbarFilterProps {
+  sortOptions?: EnumItem[];
+  searchPlaceholder?: string;
+  addLabel?: string;
+  onAdd?: () => void;
+  showFilterButton?: boolean;
+  filterOptions: FilterOption[];
+}
+
+const Toolbar = ({
+  sortOptions = [],
+  searchPlaceholder = "Пошук",
+  addLabel = "Додати",
+  onAdd,
+  showFilterButton = true,
+  filterOptions,
+}: ToolbarFilterProps) => {
+  return (
+    <div className="flex flex-col md:flex-row md:items-center md:justify-between py-4 mt-12">
+      <div className="flex flex-1 items-center gap-8 flex-wrap">
+        {sortOptions.length > 0 && <Sort options={sortOptions} />}
+        {showFilterButton && <FilterDropdown filters={filterOptions} />}
+        <SearchInput placeholder={searchPlaceholder} />
+      </div>
+
+      {onAdd && (
+        <Button
+          onClick={onAdd}
+          className="whitespace-nowrap text-xs/tight font-semibold"
+        >
+          <Plus className="size-3" />
+          {addLabel}
+        </Button>
+      )}
+    </div>
+  );
+};
+export default Toolbar;
