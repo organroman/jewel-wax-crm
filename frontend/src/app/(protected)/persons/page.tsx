07e538a644ba87e52237f51c6b9000c39a6eecb3
r.organ <r.organ@dan-it.com.ua>
2025-06-06 20:36:24 +0300

    protected persons page

--- frontend/src/app/(protected)/persons/page.tsx
+++ frontend/src/app/(protected)/persons/page.tsx
@@ -1,6 +1,23 @@
+import { PersonRoleValue } from "@/types/person.types";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
 import PersonsClient from "./persons-client";
 
+import { getRoleAndUserFromToken, hasPermission } from "@/lib/utils";
+import { PERMISSIONS } from "@/constants/permissions.constants";
+
 const PersonsPage = async () => {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value;
+
+  const role = getRoleAndUserFromToken(token as PersonRoleValue);
+
+  const permission = hasPermission(PERMISSIONS.PERSONS.VIEW, role);
+
+  if (!permission) {
+    redirect("/dashboard");
+  }
   return <PersonsClient />;
 };
 
