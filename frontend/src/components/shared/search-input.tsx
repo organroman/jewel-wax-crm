a112bb4a1a8d1bd877a381d869687dd6f3b6b878
r.organ <r.organ@dan-it.com.ua>
2025-07-14 11:21:13 +0300

    search replaced with search by type instead of enter

--- frontend/src/components/shared/search-input.tsx
+++ frontend/src/components/shared/search-input.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSearchParams, useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Search } from "lucide-react";
 
@@ -10,19 +10,21 @@ import { Input } from "../ui/input";
 interface SearchProps {
   placeholder?: string;
   param?: string;
+  delay?: number;
 }
 
 const SearchInput = ({
   placeholder = "Пошук",
   param = "search",
+  delay = 500,
 }: SearchProps) => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
   const [value, setValues] = useState<string>(searchParams.get(param) || "");
 
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
+  useEffect(() => {
+    const handler = setTimeout(() => {
       const params = new URLSearchParams(searchParams);
 
       if (value.trim()) {
@@ -30,9 +32,25 @@ const SearchInput = ({
       } else {
         params.delete(param);
       }
+
       router.push(`?${params.toString()}`);
-    }
-  };
+    }, delay);
+
+    return () => clearTimeout(handler); // clean up on re-typing
+  }, [value]);
+
+  // const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+  //   if (e.key === "Enter") {
+  //     const params = new URLSearchParams(searchParams);
+
+  //     if (value.trim()) {
+  //       params.set(param, value.trim());
+  //     } else {
+  //       params.delete(param);
+  //     }
+  //     router.push(`?${params.toString()}`);
+  //   }
+  // };
   return (
     <div className="relative w-full max-w-xs">
       <Search className="absolute left-3 top-1/2 size-4 -translate-y-1/2 text-text-light" />
@@ -40,7 +58,7 @@ const SearchInput = ({
         type="search"
         value={value}
         onChange={(e) => setValues(e.target.value)}
-        onKeyDown={handleKeyDown}
+        // onKeyDown={handleKeyDown}
         placeholder={placeholder}
         className="pl-10 bg-ui-sidebar w-[280px] placeholder:text-text-light placeholder:text-xs "
       />
