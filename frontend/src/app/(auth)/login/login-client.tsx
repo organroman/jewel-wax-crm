99b2537e2b06b7af6a0c145d687caa7f6eae97c0
r.organ <r.organ@dan-it.com.ua>
2025-08-15 15:48:53 +0300

    update login page, fix hydration bug

---
+++ frontend/src/app/(auth)/login/login-client.tsx
@@ -0,0 +1,141 @@
+"use client";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Cookies from "js-cookie";
+import { Loader } from "lucide-react";
+import { useTranslation } from "react-i18next";
+import { toast } from "sonner";
+import { useTheme } from "next-themes";
+
+import { LoginSchema, loginSchema } from "@/validators/auth.validator";
+
+import { UseAuth } from "@/api/auth/use-auth";
+
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import LogoIconDark from "@/assets/icons/logo-dark.svg";
+import LogoIconLight from "@/assets/icons/logo-light.svg";
+
+const LoginClient = () => {
+  const router = useRouter();
+  const { t } = useTranslation();
+  const { theme } = useTheme();
+
+  const form = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const loginMutation = UseAuth.login();
+
+  const onSubmit = (formData: LoginSchema) => {
+    loginMutation.mutate(formData, {
+      onSuccess: ({ token }) => {
+        Cookies.set("token", token);
+        toast.success(t("messages.success.welcome_back"));
+
+        router.push("/dashboard");
+      },
+      onError: (error) => {
+        toast.error(error.message || "Failed to login");
+      },
+    });
+  };
+
+  return (
+    <Card className="w-full h-full md:w-[487px] shadow-md">
+      <CardHeader className="flex flex-col gap-6 items-center justify-center text-center p-6">
+        {theme === "dark" ? (
+          <LogoIconDark className="self-center" />
+        ) : (
+          <LogoIconLight className="self-center" />
+        )}
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      name="email"
+                      placeholder={t("placeholders.email")}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>{t("labels.password")}</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="password"
+                      name="password"
+                      placeholder={t("placeholders.password")}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-xs" />
+                </FormItem>
+              )}
+            />
+            <div className="w-full">
+              <Button
+                className="w-full mt-6 bg-brand-default hover:opacity-80 transition"
+                size="lg"
+                disabled={loginMutation.isPending}
+              >
+                {loginMutation.isPending ? (
+                  <div className="flex flex-row">
+                    <Loader className="size-6 animate-spin text-white mr-2" />
+                    <span>{t("buttons.please_wait")}...</span>
+                  </div>
+                ) : (
+                  "Login"
+                )}
+              </Button>
+              {loginMutation.isError && (
+                <span className="text-sm text-action-alert">
+                  {loginMutation.error.message}
+                </span>
+              )}
+            </div>
+          </form>
+        </Form>
+        <p className="mt-5 text-sm text-text-regular text-center">
+          {t("auth.forget_password")}?
+          <Link href="/reset-password-token">
+            <span className="text-brand-default font-bold underline hover:opacity-80 transition">
+              &nbsp;{t("auth.reset_password")}
+            </span>
+          </Link>
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default LoginClient;
