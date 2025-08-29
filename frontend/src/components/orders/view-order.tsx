b841d628654f3d371d02696c698720dc9e1a9e02
r.organ <r.organ@dan-it.com.ua>
2025-08-29 17:33:37 +0300

    badge in view order component

--- frontend/src/components/orders/view-order.tsx
+++ frontend/src/components/orders/view-order.tsx
@@ -1,4 +1,3 @@
-import { useTranslation } from "react-i18next";
 import { Loader } from "lucide-react";
 
 import { useOrder } from "@/api/order/use-order";
@@ -8,14 +7,15 @@ import { Button } from "../ui/button";
 import { Dialog } from "../ui/dialog";
 import Modal from "../shared/modal/modal";
 import OrderInfo from "./order-info";
+import { Badge } from "../ui/badge";
 
 interface ViewOrderProps {
   orderId: number;
   orderNumber: number;
+  badge?: number;
 }
 
-const ViewOrder = ({ orderId, orderNumber }: ViewOrderProps) => {
-  const { t } = useTranslation();
+const ViewOrder = ({ orderId, orderNumber, badge }: ViewOrderProps) => {
   const { dialogOpen, setDialogOpen } = useDialog();
   const {
     data: order,
@@ -25,13 +25,20 @@ const ViewOrder = ({ orderId, orderNumber }: ViewOrderProps) => {
 
   return (
     <div>
-      <Button
-        variant="ghost"
-        onClick={() => setDialogOpen(true)}
-        className="underline text-action-plus hover:bg-transparent hover:text-indigo-400 font-medium  text-xs cursor-pointer"
-      >
-        {orderNumber}
-      </Button>
+      <div className="flex flex-row items-center gap-1">
+        <Button
+          variant="ghost"
+          onClick={() => setDialogOpen(true)}
+          className="flex pr-2 underline text-action-plus hover:bg-transparent hover:text-indigo-400 font-medium  text-xs cursor-pointer"
+        >
+          {orderNumber}
+        </Button>
+        {badge && (
+          <Badge className="bg-brand-default rounded-full w-5 h-5 text-[10px] font-bold">
+            {badge}
+          </Badge>
+        )}
+      </div>
       <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
         <Modal hideClose dialogContentClassname="p-0 lg:p-0 lg:pr-0">
           {error && <p>{error.message}</p>}
